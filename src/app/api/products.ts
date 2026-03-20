7431f168118242fd0d85ad681f550c69c3fae60b
Ignacio Lopez Amat <nacholopezamat@gmail.com>
2026-03-20 17:06:59 +0100

    Ajustado limite de la api

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -3,7 +3,7 @@ import type {ProductApi, Producto} from "../types/product";
 import {api} from "./api";
 
 export const getProducts = async (): Promise<ProductApi> => {
-    const response = await api.get<ProductApi>("products");
+    const response = await api.get<ProductApi>("products?limit=194");
     return response.data;
 }
 
